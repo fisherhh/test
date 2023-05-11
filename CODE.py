fcc04c1dbe0ef76449d9da533e784315c84a57e3
fisherhh <53724273+fisherhh@users.noreply.github.com>
2023-05-11 13:53:13 +0800

    Create CODE.py

---
+++ CODE.py
@@ -0,0 +1,9 @@
+config = open_api_models.Config(
+    # 必填，您的 AccessKey ID,
+    access_key_id='LTAI5tLMXaa5ccTFubMf28vK',
+    # 必填，您的 AccessKey Secret,
+    access_key_secret='DcQssRDuLItGXWx5foiuleWWeFsfsM'
+  
+  
+  
+  this for test
