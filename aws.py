6beb5ed4155cc6df51054b5faf7919932b687658
fisherhh <53724273+fisherhh@users.noreply.github.com>
2023-05-11 13:57:17 +0800

    Create aws.py

---
+++ aws.py
@@ -0,0 +1,5 @@
+[root@jumpserver ~]# aws configure
+AWS Access Key = AKIAY4KNBP5FL67BOY7Q
+AWS Secret Access Key = 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
+Default region name [None]: ap-northeast-1
+Default output format [None]: json
